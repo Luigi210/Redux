43fa869344e695f41c508784c0011de0037e9b40
Assan21 <65251749+Luigi210@users.noreply.github.com>
2021-11-19 00:55:39 +0600

    refactor: Redux app

--- redux-app/src/App.js
+++ redux-app/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
 import './App.css';
 
-function App() {
+function App(props) {
+
+
+  const addCounter = () => {
+    // setCounter(counter + 1);
+  }
+
+  const delCounter = () => {
+    // setCounter(counter - 1);
+  }
+  console.log('APP', props);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Click to change</h2>
+      <p>{props.counter}</p>
+      <button onClick={props.onAdd}>+</button>
+        <button onClick={props.onSub}>-</button><br></br>
+        <button onClick={() => props.onAddNum(2)}>+2</button>
+        <button onClick={() => props.onAddNum(3)}>+3</button>
+        <button onClick={() => props.onAddNum(4)}>+4</button>
+        <button onClick={() => props.onAddNum(5)}>+5</button>
+        <button onClick={() => props.onAddNum(6)}>+6</button>
+        <button onClick={() => props.onAddNum(7)}>+7</button>
+
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state){
+    return {
+        counter: state.counter
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        onAdd: () => dispatch({ type: 'ADD'}),
+        onSub: () => dispatch({ type: 'SUB'}),
+        onAddNum: (number) => dispatch({ type: 'ADD_NUM', value: number}),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
